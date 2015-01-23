9c8869ad38b37a48a1c6c2b64f81fd12d9562a64
KayneWest <mkrzus3@gmail.com>
2015-01-22 18:06:23 -0600

    Create CrawlBeast2.py
    includes farmers markets, specialty shops, and grocery stores

---
+++ CrawlBeast2.py
@@ -0,0 +1,120 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import random
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+import random
+import datetime
+import sys
+
+html_escape_table = {
+    "&amp;": "&",
+    "&quot;":'"',
+    "&apos;":"'",
+    "&gt;":">",
+     "&lt;":"<",
+    }
+
+def html_escape(text):
+    """Produce entities within text."""
+    return " ".join(html_escape_table.get(c,c) for c in text.split())
+
+def log(msg):
+    print("{} {}".format(str(datetime.datetime.now()), msg))
+
+def removeNonAscii(s): 
+    return "".join(filter(lambda x: ord(x)<128, s))
+
+class GetSalesLists(object):
+    def __init__(self):
+        self.driver=webdriver.PhantomJS()
+        self.company_names=[]
+
+    def grocery(self):
+        log("grabbing initial 'Grocery' page")
+        self.grocery_html=[]
+        self.driver.get('http://www.yelp.com/chicago')
+        blue=self.driver.find_elements_by_css_selector('input#find_desc')[0]
+        blue.send_keys('grocery')
+        time.sleep(2)
+        blue.send_keys(u'\ue015')
+        time.sleep(2)
+        blue.send_keys(u'\ue015')
+        time.sleep(2)
+        blue.send_keys(u'\ue007')
+        time.sleep(10)
+        self.grocery_html.append(self.driver.page_source)
+
+        for i in range(10,200,10):
+            num=random.randint(5,10)
+            print 'waiting for '+ str(num)+' seconds'
+            time.sleep(num)
+            log('getting another link')
+            link="http://www.yelp.com/search?find_desc=Grocery&find_loc=Chicago%2C+IL&start="+str(i)
+            self.driver.get(link)
+            self.grocery_html.append(self.driver.page_source)
+
+    def specialty_food(self):
+        self.specialty_food_html=[]
+        log("grabbing initial 'specialty_food' page")
+        self.driver.get('http://www.yelp.com/search?find_desc=Specialty+Food&find_loc=Chicago%2C+IL&ns=1')
+        self.grocery_html.append(self.driver.page_source)
+
+        for i in range(10,200,10):
+            num=random.randint(5,10)
+            print 'waiting for '+ str(num)+' seconds'
+            time.sleep(num)
+            log('getting another link')
+            link="http://www.yelp.com/search?find_desc=Specialty+Food&find_loc=Chicago%2C+IL&ns=1#start="+str(i)
+            self.driver.get(link)
+            self.specialty_food.append(self.driver.page_source)
+
+    def farmersmarket(self):
+        self.farmersmarket_html()
+        log("grabbing initial 'farmersmarket' page")
+        self.driver.get('http://www.yelp.com/search?find_desc=Farmers+Market&find_loc=Chicago%2C+IL&ns=1')
+        self.grocery_html.append(self.driver.page_source)
+
+        for i in range(10,200,10):
+            num=random.randint(5,10)
+            print 'waiting for '+ str(num)+' seconds'
+            time.sleep(num)
+            log('getting another link')
+            link="http://www.yelp.com/search?find_desc=Farmers+Market&find_loc=Chicago%2C+IL&ns=1#start="+str(i)
+            self.driver.get(link)
+            self.farmersmarket_html.append(self.driver.page_source)
+
+    def run_all(self):
+        self.grocery()
+        self.specialty_food()
+        self.farmersmarket()
+        self.get_names(self.grocery_html)
+        self.get_names(self.specialty_food_html)
+        self.get_names(self.farmersmarket_html)
+
+
+    def get_names(self,from_where):
+        for html in from_where:
+            matches=re.findall(r'data-hovercard-id.*</a>',html)
+            for match in matches:
+                item=html_escape(removeNonAscii(re.findall(r'>.*</a>',match)[0][1:-4]))
+                self.company_names.append(item)
+        self.company_names=set(self.company_names) #saves on mem space
+
+    def list_names(self,argv):
+        """ need to list a file name """
+        print self.company_names
+        with open('saleslist.txt','wb') as f:
+            for company in self.company_names:
+                f.write(company+'\t')
+
+def main(argv):
+    g=GetSalesLists()
+    g.run_all()
+    g.list_names(argv)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
